6ba60610684e7b1d98a92c45cac3bf9fc3ad6263
Krzysztof Witukiewicz <krzysztof1980@users.noreply.github.com>
2017-09-17 18:14:29 +0200

    Disable dynamic mapping in the index
    Dynamic mapping caused too many fields to be indexed. As an alternative, the property index.mapping.total_fields.limit could be set to value > 1000, but it should be not needed.

--- src/main/groovy/wiktiopeggynary/persistence/WiktionaryEntryEsRepository.groovy
+++ src/main/groovy/wiktiopeggynary/persistence/WiktionaryEntryEsRepository.groovy
@@ -1,5 +1,8 @@
 package wiktiopeggynary.persistence
 
+import org.apache.http.HttpEntity
+import org.apache.http.entity.ContentType
+import org.apache.http.nio.entity.NStringEntity
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import pl.kwitukiewicz.wdb.elasticsearch.ElasticsearchIndexingRepository
@@ -8,7 +11,7 @@ import wiktiopeggynary.model.WiktionaryEntry
 /**
  * @author Krzysztof Witukiewicz
  */
-class WiktionaryEntryEsRepository extends ElasticsearchIndexingRepository {
+class WiktionaryEntryEsRepository extends ElasticsearchIndexingRepository<WiktionaryEntry> {
     private static final Logger logger = LoggerFactory.getLogger(WiktionaryEntryEsRepository.class)
 
     private static final String WIKTIONARY_ENTRY_INDEX = "dewiktionary_entries"
@@ -23,7 +26,38 @@ class WiktionaryEntryEsRepository extends ElasticsearchIndexingRepository {
     }
 
     @Override
+    void prepareIndexForRebuild() {
+        super.prepareIndexForRebuild()
+        def jsonString = """\
+{
+    "dynamic": false,
+    "properties": {
+        "lemma": {
+            "type": "text",
+            "fields": {
+                "keyword": {
+                    "type": "keyword"
+                }
+            }
+        }
+    }
+}
+"""
+        HttpEntity entity = new NStringEntity(jsonString, ContentType.APPLICATION_JSON)
+        try {
+            lowLevelClient.performRequest("PUT", "${index}/_mapping/${type}", Collections.emptyMap(), entity)
+        } catch (Exception e) {
+            throw new RuntimeException("Error setting mappings in ${index}", e)
+        }
+    }
+
+    @Override
     protected Logger getLogger() {
         return logger
+    }
+
+    @Override
+    protected String createIdForObject(WiktionaryEntry entry) {
+        return UUID.randomUUID()
     }
 }
